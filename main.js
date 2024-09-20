70923ff6c7d5434c09dce43a84ebd419fec47108
advait0012 <advait0012@proton.me>
2024-09-21 02:09:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,25 +3,22 @@ const inputEl = document.querySelector(".display-password");
 const buttonEl = document.querySelector(".generator");
 
 let chars = "abcdefghijklmnopqstuvwxyz1234567890!@#$&_";
-let pass = "";
 let lengthVar;
 
-buttonEl.addEventListener("click", () => {
-  let lengthVar = lengthEl.value;
-  for (let i = 0; i < lengthVar; i++) {
-    let random = Math.floor(Math.random() * chars.length);
-    pass += chars[random];
-  }
-  inputEl.value = pass;
-});
+function genPassword(){
+    let pass = "";
+    let lengthVar = lengthEl.value;
+      for (let i = 0; i < lengthVar; i++) {
+        let random = Math.floor(Math.random() * chars.length);
+        pass += chars[random];
+      }
+      inputEl.value = pass;
+}
+
+buttonEl.addEventListener("click",genPassword);
 
 lengthEl.addEventListener("keypress", (e) => {
   if (e.keyCode === 13) {
-    let lengthVar = lengthEl.value;
-    for (let i = 0; i < lengthVar; i++) {
-      let random = Math.floor(Math.random() * chars.length);
-      pass += chars[random];
-    }
-    inputEl.value = pass;
+    genPassword()
   }
 });
