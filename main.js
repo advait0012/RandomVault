c12a1fade25bf22e2ff0365533cb483f3ba6dcbb
advait0012 <advait0012@proton.me>
2024-09-25 01:14:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-// const lengthEl = document.querySelector(".password-length");
-// const inputEl = document.querySelector(".display-password");
-// const buttonEl = document.querySelector(".generator");
-
-// let chars = "abcdefghijklmnopqstuvwxyz1234567890!@#$&_";
-// let lengthVar;
-
-// function genPassword() {
-//   let pass = "";
-//   let lengthVar = lengthEl.value;
-//   for (let i = 0; i < lengthVar; i++) {
-//     let random = Math.floor(Math.random() * chars.length);
-//     pass += chars[random];
-//   }
-//   inputEl.value = pass;
-// }
-
-// buttonEl.addEventListener("click", genPassword);
-
-// lengthEl.addEventListener("keypress", (e) => {
-//   if (e.keyCode === 13) {
-//     genPassword();
-//   }
-// });
 const lengthEl = document.querySelector(".password-length");
 const inputEl = document.querySelector(".display-password");
 const buttonEl = document.querySelector(".generator");
@@ -29,96 +5,125 @@ const charsEl = document.querySelector(".chars-input");
 const numEl = document.querySelector(".num-input");
 const specialEl = document.querySelector(".special-input");
 
-// let chars = "abcdefghijklmnopqstuvwxyz1234567890!@#$&_";
-// let lengthVar;
-
-// function genPassword() {
-//   let pass = "";
-//   let lengthVar = lengthEl.value;
-//   for (let i = 0; i < lengthVar; i++) {
-//     let random = Math.floor(Math.random() * chars.length);
-//     pass += chars[random];
-//   }
-//   inputEl.value = pass;
-// }
-
-// buttonEl.addEventListener("click", genPassword);
-
-// lengthEl.addEventListener("keypress", (e) => {
-//   if (e.keyCode === 13) {
-//     genPassword();
-//   }
-// });
-
 let char = "abcdefghijklmnopqrstuvwxyz";
 let num = "0123456789";
 let special = "(!@#$%^&*)";
-let charNum = "abcdefghijklmnopqrstuvwxyz1234567890";
-let charsSpecial = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()"
+
+function randomCharFunc() {
+  let randomChar = "";
+  let charRandom = Math.floor(Math.random() * char.length);
+  return (randomChar += char[charRandom]);
+}
+
+function randomNumFunc() {
+  let randomNum = "";
+  let numRandom = Math.floor(Math.random() * num.length);
+  return (randomNum += num[numRandom]);
+}
+
+function randomspecialFunc() {
+  let randomspecial = "";
+  let specialRandom = Math.floor(Math.random() * special.length);
+  return (randomspecial += special[specialRandom]);
+}
+
+function randomCharNum() {
+  let charNumRandom = "";
+  const charNum = Math.random() < 0.5;
+  if (charNum) {
+    const randomCharNum = Math.floor(Math.random() * char.length);
+    charNumRandom += char[randomCharNum];
+  } else {
+    const randomCharNum = Math.floor(Math.random() * num.length);
+    charNumRandom += num[randomCharNum];
+  }
+  return charNumRandom;
+}
+
+function randomSpecialNum() {
+  let numSpecialRandom = "";
+  const specialNum = Math.random() < 0.5;
+  if (specialNum) {
+    const specialNumRandom = Math.floor(Math.random() * num.length);
+    numSpecialRandom += num[specialNumRandom];
+  } else {
+    const specialNumRandom = Math.floor(Math.random() * special.length);
+    numSpecialRandom += special[specialNumRandom];
+  }
+  return numSpecialRandom;
+}
+
+function randomSpecialChar() {
+  let charSpecialRandom = "";
+  const specialChar = Math.random() < 0.5;
+  if (specialChar) {
+    const specialCharRandom = Math.floor(Math.random() * char.length);
+    charSpecialRandom += char[specialCharRandom];
+  } else {
+    const specialCharRandom = Math.floor(Math.random() * special.length);
+    charSpecialRandom += special[specialCharRandom];
+  }
+  return charSpecialRandom;
+}
+
+function randomSpecialCharNum() {
+  let charNumSpecialRandom = "";
+  const specialCharNum = Math.random() < 0.3;
+  if (specialCharNum) {
+    const specialCharNumRandom = Math.floor(Math.random() * char.length);
+    charNumSpecialRandom += char[specialCharNumRandom];
+  } if(specialCharNum){
+    const specialCharNumRandom = Math.floor(Math.random() * special.length);
+    charNumSpecialRandom += special[specialCharNumRandom];
+  }else{
+    const specialCharNumRandom = Math.floor(Math.random() * num.length);
+    charNumSpecialRandom += num[specialCharNumRandom];
+  }
+  return charNumSpecialRandom;
+}
 
 buttonEl.addEventListener("click", () => {
-  if (charsEl.checked) {
-    let pass = "";
+  let newRandom = [];
+  if (charsEl.checked && !numEl.checked && !specialEl.checked) {
     for (let i = 0; i < lengthEl.value; i++) {
-      let random = Math.floor(Math.random() * char.length);
-      pass += char[random];
+      newRandom.push(randomCharFunc());
     }
-    inputEl.value = pass;
   }
-});
 
-buttonEl.addEventListener("click", () => {
-  if (numEl.checked) {
-    let pass = "";
+  if (numEl.checked && !charsEl.checked && !specialEl.checked) {
     for (let i = 0; i < lengthEl.value; i++) {
-      let random = Math.floor(Math.random() * num.length);
-      pass += num[random];
+      newRandom.push(randomNumFunc());
     }
-    inputEl.value = pass;
   }
-});
 
-buttonEl.addEventListener("click", () => {
-  if (specialEl.checked) {
-    let pass = "";
+  if (specialEl.checked && !numEl.checked && !charsEl.checked) {
     for (let i = 0; i < lengthEl.value; i++) {
-      let random = Math.floor(Math.random() * special.length);
-      pass += special[random];
+      newRandom.push(randomspecialFunc());
     }
-    inputEl.value = pass;
   }
-});
 
-buttonEl.addEventListener("click", () => {
-  if (charsEl.checked && numEl.checked) {
-    let pass = "";
+  if (charsEl.checked && numEl.checked && !specialEl.checked) {
     for (let i = 0; i < lengthEl.value; i++) {
-      let random = Math.floor(Math.random() * charNum.length);
-      pass += charNum[random]
+      newRandom.push(randomCharNum());
     }
-    inputEl.value = pass;
   }
-});
 
-buttonEl.addEventListener("click", () => {
-  if (charsEl.checked && specialEl.checked) {
-    let pass = "";
+  if (numEl.checked && specialEl.checked && !charsEl.checked) {
     for (let i = 0; i < lengthEl.value; i++) {
-      let random = Math.floor(Math.random() * charsSpecial.length);
-      pass += charsSpecial[random]
+      newRandom.push(randomSpecialNum());
     }
-    inputEl.value = pass;
   }
-});
 
-let specialNums = "1234567890!@#$%^&*()"
-buttonEl.addEventListener("click", () => {
-  if (specialEl.checked && numEl.checked) {
-    let pass = "";
+  if (charsEl.checked && specialEl.checked && !numEl.checked) {
     for (let i = 0; i < lengthEl.value; i++) {
-      let random = Math.floor(Math.random() * specialNums.length);
-      pass += specialNums[random]
+      newRandom.push(randomSpecialChar());
     }
-    inputEl.value = pass;
   }
+  if (charsEl.checked && specialEl.checked && numEl.checked) {
+    for (let i = 0; i < lengthEl.value; i++) {
+      newRandom.push(randomSpecialCharNum() );
+    }
+  }
+
+  inputEl.value = newRandom.join("");
 });
